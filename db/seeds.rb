a15d0d2512e0f30ab9ca185d2c9bee4c1248986f
kanbaru-github <usupfv@gmail.com>
2021-07-07 03:19:11 +0000

    add_seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+  List.create!(
+    title: 'railsを学ぼう',
+    body: 'seedファイルについて'
+  )
+  # データベースのデータを削除した後に内部的にrails db:seedを実行して初期データを挿入する
+
+  10.times do |number|
+    List.create!(title: 'timeを学習しよう',body: number)
+  end
+  # timesメソッドを用いて、seedファイルでは同時に複数のデータを作成する事ができる
